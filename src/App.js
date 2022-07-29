f8e0493a9c0f9a55aff7d3ea7de7261ec97878f2
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-07-29 14:33:15 +0200

    components files init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./dist/css/App.css";
+
+// pages
+import HomePage from "./pages/HomePage";
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
