bfb8ccb28a86502ce18fa657ba2f5c36e4ac657f
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-07-30 15:51:22 +0200

    styled navigation button

--- src/components/HeroComponent.js
+++ src/components/HeroComponent.js
@@ -27,7 +27,23 @@ export default function HeroComponent() {
             </h4>
             <div className="hero-blog-link-bg"></div>
             <p className="hero-blog-link--sub-title"></p>
-            <button className="wl-btn wl-btn-navigation">next</button>
+            <div className="wl-btn wl-btn--navigation wl-btn--navigation--text">
+              <span>next</span>
+              <svg
+                className=".wl-btn--navigation__icon"
+                xmlns="http://www.w3.org/2000/svg"
+                width="12"
+                height="12"
+                fill="currentColor"
+                class="bi bi-chevron-right"
+                viewBox="0 0 10 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+                />
+              </svg>
+            </div>
           </div>
         </a>
       </div>
