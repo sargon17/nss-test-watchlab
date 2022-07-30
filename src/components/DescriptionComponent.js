ffc237b1784302da99fbfda587170d982091085a
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-07-30 16:59:12 +0200

    description section layout done

---
+++ src/components/DescriptionComponent.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function DescriptionComponent() {
+  return (
+    <div className="container">
+      <div className="desc-container">
+        <h1 className="wl-title">
+          History and <br /> Patriarcal season
+        </h1>
+        <div className="desc-content grid">
+          <div>
+            <p>
+              On the other hand, we denounce with righteous indigation and
+              dislike men who are so beguiled and demoralized by the charms of
+              pleasure of the moment, so blinded by desire
+            </p>
+          </div>
+          <div>
+            <p>
+              But I must explain to you how all this mistaken idea of denouncing
+              pleasure and praising pain was born and I will give you a complete
+              account of the system, and expound the actual teachings of the
+              great explorer of the truth, the master-builder of human
+              happiness. No that are extremely painful
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
