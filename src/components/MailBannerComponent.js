0ae43f859d4506f95b6b86ef0101477d2e79381b
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-07-31 00:06:57 +0200

    mail banner done

---
+++ src/components/MailBannerComponent.js
@@ -0,0 +1,32 @@
+import Mail from "../assets/imgs/Store-2.jpg";
+
+export default function MailBannerComponent() {
+  return (
+    <div className="container">
+      <div className="mail-container">
+        <div className="mail-overlay"></div>
+        <img src={Mail} alt="mail" />
+        <div className="mail-content text-center">
+          <div>
+            <h1 className="wl-title wl-title-small text-color-white">
+              Get in Touch
+            </h1>
+            <h5 className="wl-pretitle text-color-white">
+              Discover more about news, <br /> exclusive events and discount
+            </h5>
+            <div className="mail-form">
+              <input
+                className="wl-input wl-input--primary wl-input--primary--white"
+                type="text"
+                placeholder="type your email"
+              />
+              <button className="wl-btn wl-btn--primary wl-btn--primary--white wl-btn--small">
+                ok
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
