f6ffd6221f610d98b814ed556956beabbc9de6f1
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-07-30 20:06:00 +0200

    solved layout bug

--- src/components/DescriptionComponent.js
+++ src/components/DescriptionComponent.js
@@ -8,22 +8,18 @@ export default function DescriptionComponent() {
           History and <br /> Patriarcal season
         </h1>
         <div className="desc-content grid">
-          <div>
-            <p className="wl-text">
-              On the other hand, we denounce with righteous indigation and
-              dislike men who are so beguiled and demoralized by the charms of
-              pleasure of the moment, so blinded by desire
-            </p>
-          </div>
-          <div>
-            <p className="wl-text">
-              But I must explain to you how all this mistaken idea of denouncing
-              pleasure and praising pain was born and I will give you a complete
-              account of the system, and expound the actual teachings of the
-              great explorer of the truth, the master-builder of human
-              happiness. No that are extremely painful
-            </p>
-          </div>
+          <p className="wl-text">
+            On the other hand, we denounce with righteous indigation and dislike
+            men who are so beguiled and demoralized by the charms of pleasure of
+            the moment, so blinded by desire
+          </p>
+          <p className="wl-text">
+            But I must explain to you how all this mistaken idea of denouncing
+            pleasure and praising pain was born and I will give you a complete
+            account of the system, and expound the actual teachings of the great
+            explorer of the truth, the master-builder of human happiness. No
+            that are extremely painful
+          </p>
         </div>
       </div>
     </div>
