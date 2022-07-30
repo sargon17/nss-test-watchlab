40069c8fd3e14afc9c2cdd12f7560d31e52a91aa
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-07-30 18:39:13 +0200

    added new classes

--- src/components/DescriptionComponent.js
+++ src/components/DescriptionComponent.js
@@ -9,14 +9,14 @@ export default function DescriptionComponent() {
         </h1>
         <div className="desc-content grid">
           <div>
-            <p>
+            <p className="wl-text">
               On the other hand, we denounce with righteous indigation and
               dislike men who are so beguiled and demoralized by the charms of
               pleasure of the moment, so blinded by desire
             </p>
           </div>
           <div>
-            <p>
+            <p className="wl-text">
               But I must explain to you how all this mistaken idea of denouncing
               pleasure and praising pain was born and I will give you a complete
               account of the system, and expound the actual teachings of the
