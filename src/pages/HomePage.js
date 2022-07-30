270efa6350d021e293d3c6a155d2a058b30798ec
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-07-30 17:02:32 +0200

    Description component added to home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,11 +2,13 @@ import React from "react";
 
 // components
 import HeroComponent from "../components/HeroComponent";
+import DescriptionComponent from "../components/DescriptionComponent";
 
 export default function HomePage() {
   return (
     <div>
       <HeroComponent />
+      <DescriptionComponent />
     </div>
   );
 }
