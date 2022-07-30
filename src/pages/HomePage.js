93c7f04fe3e493257d849a289fa316fb25cc78ed
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-07-30 18:40:42 +0200

    added new section

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,12 +3,14 @@ import React from "react";
 // components
 import HeroComponent from "../components/HeroComponent";
 import DescriptionComponent from "../components/DescriptionComponent";
+import ImgSideComponent from "../components/ImgSideComponent";
 
 export default function HomePage() {
   return (
     <div>
       <HeroComponent />
       <DescriptionComponent />
+      <ImgSideComponent />
     </div>
   );
 }
