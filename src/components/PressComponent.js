24813d465a249756d280985754765f0c1a1e52eb
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-08-03 10:43:48 +0200

    added slider functionality

--- src/components/PressComponent.js
+++ src/components/PressComponent.js
@@ -1,11 +1,55 @@
 import CoverImg from "../assets/imgs/watch_manufactort.jpg";
+import Mail from "../assets/imgs/Store-2.jpg";
+import Watch3 from "../assets/imgs/London-skyline.jpg";
+
 import { useInView } from "react-cool-inview";
+
+import { useState } from "react";
 
 export default function PressComponent() {
   const { observe, inView } = useInView({
     unobserveOnEnter: true,
     rootMargin: "-400px 0px",
   });
+
+  // creating a slider for the press
+  const [slider, setSlider] = useState(0);
+  const sliderContent = [
+    {
+      title: "Most Important Days on Watchlab",
+      data: "18 September 2020",
+      img: CoverImg,
+    },
+    {
+      title: "Watch Manufacturer",
+      data: "18 September 2020",
+      img: Mail,
+    },
+    {
+      title: "Watch Manufacturer",
+      data: "18 September 2020",
+      img: Watch3,
+    },
+  ];
+
+  const nextSlide = () => {
+    setSlider((prevSlider) => {
+      if (prevSlider < sliderContent.length - 1) {
+        return prevSlider + 1;
+      } else {
+        return 0;
+      }
+    });
+  };
+  const prevSlide = () => {
+    setSlider((prevSlider) => {
+      if (prevSlider > 0) {
+        return prevSlider - 1;
+      } else {
+        return sliderContent.length - 1;
+      }
+    });
+  };
 
   return (
     <div className="container" ref={observe}>
@@ -32,13 +76,33 @@ export default function PressComponent() {
           }
         >
           <div className="press__slider--container">
-            <img src={CoverImg} width="400px" />
-            <div className="press__slider--content">
+            <div className="press__slider-img">
+              <img src={sliderContent[slider].img} width="400px" />
+            </div>
+            <div className="press__slider-content">
               <h5 className="wl-pretitle">press</h5>
-              <h6 className="wl-pretitle text-color-gold">18 settembre 2015</h6>
+              <h6 className="wl-pretitle text-color-gold">
+                {sliderContent[slider].data}
+              </h6>
               <h2 className="wl-title wl-title--small">
-                Most Important Days on Watchlab
+                {sliderContent[slider].title}
               </h2>
+              <div className="press__slider-content-btns">
+                <button
+                  className="wl-btn wl-btn-icon wl-btn-icon--transparent"
+                  onClick={prevSlide}
+                >
+                  {" "}
+                  {"<"}{" "}
+                </button>
+                <button
+                  className="wl-btn wl-btn-icon wl-btn-icon--transparent"
+                  onClick={nextSlide}
+                >
+                  {" "}
+                  {">"}{" "}
+                </button>
+              </div>
             </div>
           </div>
         </div>
